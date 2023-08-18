9043f1bc2326751f7f610523e1c1fa60c567ac2c
Alek Michelson <am7590@rit.edu>
2023-08-17 21:59:21 -0400

    Initial Commit

---
+++ grpc-demo-client/grpc_demo_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  grpc_demo_clientApp.swift
+//  grpc-demo-client
+//
+//  Created by Alek Michelson on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct grpc_demo_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
