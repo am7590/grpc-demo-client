0c44a595044078dfda981a55272d1d28560fbb1f
Alek Michelson <am7590@rit.edu>
2023-09-07 21:42:21 -0400

    FINALLY GOT FUCKING GRPC TO GENERATE SHIT WHY WAS THIS SO HARD

---
+++ protoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+protoc store.proto \
+  --plugin=$(brew --prefix)/opt/swift-protobuf/bin/protoc-gen-swift \
+  --swift_opt=Visibility=Public \
+  --swift_out=$SCRIPT_DIR/grpc-demo-client/protobuf \
+  --plugin=$(brew --prefix)/opt/grpc-swift/bin/protoc-gen-grpc-swift \
+  --grpc-swift_opt=Client=true \
+  --grpc-swift_opt=Visibility=Public \
+  --grpc-swift_out=$SCRIPT_DIR/grpc-demo-client/protobuf \
+  --proto_path=. \
+  -I $SCRIPT_DIR/.. \
+
